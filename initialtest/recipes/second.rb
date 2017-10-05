bbadfd84edc0bea77502ccd3423e265b7d4a11a8
zentgrafdan <zentgrafdan@gmail.com>
2017-10-04 22:36:04 -0500

    dded second recipe to test runlists on server

---
+++ initialtest/recipes/second.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: initialtest
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#
+file '/opt/dansecond.txt' do
+  content "another file for runlist test"
+end
