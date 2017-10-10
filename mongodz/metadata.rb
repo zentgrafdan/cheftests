449a5d88e1fb298c4efc671284cc85c2fa0057d5
zentgrafdan <zentgrafdan@gmail.com>
2017-10-09 22:48:55 -0500

    changed version number

--- mongodz/metadata.rb
+++ mongodz/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'zentgrafdan@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mongoDB - for danz experiment'
 long_description 'Installs/Configures mongoDB for danz experiment'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
