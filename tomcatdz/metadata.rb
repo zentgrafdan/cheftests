24c5a8e120f859f41e8e6be0164936af4b69cc26
zentgrafdan <zentgrafdan@gmail.com>
2017-10-10 19:47:40 -0500

    adding user and group to system via tomcat recipe

--- tomcatdz/metadata.rb
+++ tomcatdz/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures tomcatdz'
 long_description 'Installs/Configures tomcatdz'
-version '0.1.0'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -18,3 +18,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/tomcatdz'
+#
+# 0.1.0 - create group
+# 0.1.1 - create user
+# 0.1.2 - added group param to user
--- tomcatdz/recipes/default.rb
+++ tomcatdz/recipes/default.rb
@@ -3,3 +3,35 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+#remote_file '/var/www/customers/public_html/index.html' do
+#  source 'http://somesite.com/index.html'
+#  owner 'web_admin'
+#  group 'web_admin'
+#  mode '0755'
+#  action :create
+#end
+
+
+#user_home = "/home/#{node['cookbook_name']['user']}"
+
+#user node['cookbook_name']['user'] do
+#  gid node['cookbook_name']['group']
+##  shell '/bin/bash'
+#  home user_home
+#  system true
+#  action :create
+#end
+
+group 'tomcat85' do
+  action :create
+  append true
+end
+
+user 'tomcat85' do
+  system true
+  action :create
+  home '/opt/tomcat85'
+  shell '/sbin/nologin'
+  group 'tomcat85'
+end
